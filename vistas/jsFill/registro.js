e090967ef6f4c8953b438f6df148b31dc8e35475
brayan <bemq1605@gmail.com>
2021-06-01 16:22:24 -0500

    hola

--- vistas/jsFill/registro.js
+++ vistas/jsFill/registro.js
@@ -1,4 +1,5 @@
 const $FORM = document.querySelector("#formRegistro");
+const $ERROR = document.querySelector("#error-char");
 $FORM.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -7,14 +8,28 @@ $FORM.addEventListener("submit", (e) => {
     const replyPass = e.target.Rpass.value;
 
     if (pass === replyPass) {
-        auth.createUserWithEmailAndPassword(email, pass).then(
-            (userCredential) => {
-                if (userCredential) {
-                    alert("usuario creado, inicie sesion con sus credenciales");
-                    window.location.href = "./login.html";
-                }
+        const createUser = async () => {
+            try {
+                await auth
+                    .createUserWithEmailAndPassword(email, pass)
+                    .then((userCredential) => {
+                        if (userCredential) {
+                            alert(
+                                "usuario creado, inicie sesion con sus credenciales"
+                            );
+                            window.location.href = "./login.html";
+                        }
+                    });
+            } catch (error) {
+                $ERROR.classList.toggle("show-error");
+                $ERROR.textContent = error.message;
+                setTimeout(() => {
+                    $ERROR.textContent = "";
+                    $ERROR.classList.toggle("show-error");
+                }, 4000);
             }
-        );
+        };
+        createUser();
     } else {
         alert("las contraseñas no son iguales");
     }
